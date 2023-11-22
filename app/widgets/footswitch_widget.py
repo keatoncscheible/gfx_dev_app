91505224b244306239a414094569d777d41e6e55
keatoncscheible <keatoncscheible@gmail.com>
2023-11-22 10:08:07 -0800

    Prevent context menu on the footswitch widget

--- app/widgets/footswitch_widget.py
+++ app/widgets/footswitch_widget.py
@@ -4,3 +4,6 @@ from PySide6.QtWidgets import QPushButton
 class FootswitchWidget(QPushButton):
     def __init__(self, parent):
         super().__init__()
+
+    def contextMenuEvent(self, event):
+        """Add context menu if needed in the future"""
