32498d0bee8054332ee50197015fe881d63ff59b
keatoncscheible <keatoncscheible@gmail.com>
2023-11-22 02:39:01 -0800

    Fix Volume pedal

--- app/pedals/volume/variants/default.py
+++ app/pedals/volume/variants/default.py
@@ -4,6 +4,6 @@ from pedals.volume.volume_pedal import VolumePedal
 
 def process_audio(pedal: VolumePedal, data: np.ndarray):
     if pedal.on_off:
-        data = (data * pedal.volume).astype(np.int16)
+        data = (data * pedal.output).astype(np.int16)
 
     return data
