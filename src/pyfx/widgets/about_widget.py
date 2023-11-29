8ede01f7549ff40c8771f69579d18ce79c3d1878
keatoncscheible <keatoncscheible@gmail.com>
2023-11-29 03:41:13 -0800

    Update documentation and fix about widget

--- src/pyfx/widgets/about_widget.py
+++ src/pyfx/widgets/about_widget.py
@@ -3,51 +3,69 @@ from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget
 
 
 class AboutWidget(QWidget):
+    """
+    A widget for displaying about information including a title, logo, and copyright message.
+    """
+
     def __init__(self):
+        """
+        Initialize the AboutWidget.
+        """
         super().__init__()
+        self.setWindowTitle("About Pedal Builder")
         self.initUI()
         self.setAttribute(Qt.WA_DeleteOnClose)
 
     def initUI(self):
+        """
+        Initializes the UI components of the AboutWidget.
+        """
         main_layout = QVBoxLayout()
         main_layout.setSpacing(20)
         main_layout.setContentsMargins(60, 20, 60, 30)
 
         # Title
-        self.title_label = QLabel("GFX Dev App", self)
-        title_font = self.title_label.font()
-        title_font.setPointSize(24)
-        title_font.setBold(True)
-        self.title_label.setFont(title_font)
+        self.title_label = QLabel("Pedal Builder", self)
+        self._configure_label_font(self.title_label, 24, True)
 
         # Logo
         self.logo_label = QLabel(self)
-        pixmap = QPixmap("app/assets/pyfx_logo.png")
+        self._set_logo("src/pyfx/assets/pyfx_logo.png")
+
+        # Copyright Message
+        self.copy_right_label = QLabel("© 2023 Keaton Scheible", self)
+        self._configure_label_font(self.copy_right_label, 16, False)
+
+        # Adding widgets to the layout
+        self._add_to_layout(main_layout)
+
+    def _configure_label_font(self, label, point_size, is_bold):
+        """
+        Configures the font for a given label.
+        :param label: The QLabel to configure.
+        :param point_size: The point size of the font.
+        :param is_bold: Whether the font should be bold.
+        """
+        font = label.font()
+        font.setPointSize(point_size)
+        font.setBold(is_bold)
+        label.setFont(font)
+
+    def _set_logo(self, logo_path):
+        """
+        Sets the logo image for the widget.
+        :param logo_path: The path to the logo image file.
+        """
+        pixmap = QPixmap(logo_path)
         scaled_pixmap = pixmap.scaled(400, 400, Qt.KeepAspectRatio)
         self.logo_label.setPixmap(scaled_pixmap)
 
-        # Copyright Message
-
-        self.copyright_label = QLabel("© 2023 Keaton Scheible", self)
-        copyright_font = self.title_label.font()
-        copyright_font.setPointSize(16)
-        copyright_font.setBold(False)
-        self.copyright_label.setFont(copyright_font)
-
-        # Adding widgets to the layout
-        main_layout.addWidget(self.title_label, alignment=Qt.AlignCenter)
-        main_layout.addWidget(self.logo_label, alignment=Qt.AlignCenter)
-        main_layout.addWidget(self.copyright_label, alignment=Qt.AlignCenter)
-        self.setLayout(main_layout)
-
-
-# For testing purposes
-if __name__ == "__main__":
-    import sys
-
-    from PySide6.QtWidgets import QApplication
-
-    app = QApplication(sys.argv)
-    about_widget = AboutWidget()
-    about_widget.show()
-    sys.exit(app.exec())
+    def _add_to_layout(self, layout):
+        """
+        Adds the title, logo, and copyright labels to the layout.
+        :param layout: The layout to which the widgets will be added.
+        """
+        layout.addWidget(self.title_label, alignment=Qt.AlignCenter)
+        layout.addWidget(self.logo_label, alignment=Qt.AlignCenter)
+        layout.addWidget(self.copy_right_label, alignment=Qt.AlignCenter)
+        self.setLayout(layout)
