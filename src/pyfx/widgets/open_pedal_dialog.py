52804b5caf505ea8dcb7e5586bbab4c4deaf66b5
keatoncscheible <keatoncscheible@gmail.com>
2023-11-30 03:27:17 -0800

    Doc and cleanup open_pedal_dialog

--- src/pyfx/widgets/open_pedal_dialog.py
+++ src/pyfx/widgets/open_pedal_dialog.py
@@ -1,37 +1,55 @@
 from pathlib import Path
 
 from PySide6.QtCore import Signal
-from PySide6.QtWidgets import QDialog, QMessageBox
+from PySide6.QtWidgets import QDialog, QListWidgetItem, QMessageBox
 
 from pyfx.ui.open_pedal_dialog_ui import Ui_OpenPedalDialog
 
 
 class OpenPedalDialog(QDialog, Ui_OpenPedalDialog):
+    """
+    A dialog for opening an existing pedal configuration.
+    Lists pedals from a specified folder and emits a signal when a pedal is selected to be opened.
+    """
+
     open_pedal = Signal(str)
 
     def __init__(self, pedal_folder: Path):
+        """
+        Initializes the OpenPedalDialog with a specified pedal folder.
+
+        :param pedal_folder: A Path object representing the folder where pedal configurations are stored.
+        """
         super().__init__()
         self.setupUi(self)
         self.pedal_folder = pedal_folder
 
+        # Populate the list with pedal names found in the pedal folder
+        self.populate_pedal_list()
+
+    def populate_pedal_list(self):
+        """Populate the list widget with the names of pedal folders."""
         pedal_names = [
-            pedal_asset_folder.name for pedal_asset_folder in pedal_folder.iterdir() if pedal_asset_folder.is_dir()
+            pedal_asset_folder.name for pedal_asset_folder in self.pedal_folder.iterdir() if pedal_asset_folder.is_dir()
         ]
         for pedal_name in pedal_names:
-            self.pedal_list.addItem(pedal_name)
+            item = QListWidgetItem(pedal_name)
+            self.pedal_list.addItem(item)
 
     def accept(self):
+        """
+        Overrides the accept method to include validation of the pedal selection.
+        Emits a signal to open the selected pedal if valid, otherwise shows a prompt.
+        """
         selected_pedal = self.pedal_list.currentItem()
         if selected_pedal:
-            pedal_to_open = selected_pedal.text()
-            self.open_pedal.emit(pedal_to_open)
+            self.open_pedal.emit(selected_pedal.text())
             super().accept()
         else:
             self.show_no_pedal_selected_prompt()
 
     def show_no_pedal_selected_prompt(self):
-        pedal_name_missing_prompt = QMessageBox()
-        pedal_name_missing_prompt.setWindowTitle("No Pedal Selected")
-        pedal_name_missing_prompt.setText("You must select a pedal to open")
-        pedal_name_missing_prompt.setStandardButtons(QMessageBox.Ok)
-        pedal_name_missing_prompt.exec_()
+        """
+        Shows a prompt indicating that a pedal must be selected to open.
+        """
+        QMessageBox.warning(self, "No Pedal Selected", "You must select a pedal to open")
