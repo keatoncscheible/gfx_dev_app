55fdd68e7cc4b4c5560877179c071d03a55f842c
keatoncscheible <keatoncscheible@gmail.com>
2023-11-30 03:19:23 -0800

    Add docs to knob_config_dialog

--- src/pyfx/widgets/knob_config_dialog.py
+++ src/pyfx/widgets/knob_config_dialog.py
@@ -7,11 +7,22 @@ from pyfx.ui.knob_config_dialog_ui import Ui_KnobConfigDialog
 
 
 class KnobConfigDialog(QDialog, Ui_KnobConfigDialog):
+    """
+    Dialog for configuring the settings of a knob, including minimum, maximum, default values,
+    precision, sensitivity, mode, and display options.
+    """
+
     def __init__(self, knob: PyFxKnob):
-        pyfx_log.debug("Knob Config Dialog opened")
+        """
+        Initializes the KnobConfigDialog with a specified knob.
+
+        :param knob: The PyFxKnob object to configure.
+        """
         super().__init__()
         self.setupUi(self)
         self.knob = knob
+
+        # Set initial state of UI elements based on the current configuration of the knob
         self.minimum_spinbox.setValue(knob.minimum_value)
         self.maximum_spinbox.setValue(knob.maximum_value)
         self.default_spinbox.setValue(knob.default_value)
@@ -19,68 +30,82 @@ class KnobConfigDialog(QDialog, Ui_KnobConfigDialog):
         self.sensitivity_spinbox.setValue(knob.sensitivity)
         self.mode_combobox.setCurrentText(knob.mode)
         self.enable_display_checkbox.setChecked(knob.display_enabled)
+
+        # Connect UI signals to methods
         self.button_box.clicked.connect(self.button_box_clicked)
         self.mode_combobox.currentTextChanged.connect(self.change_mode_settings)
 
     def button_box_clicked(self, button: QAbstractButton):
         """
-        Handles the button box click event. Updates the knob configuration based on the dialog's settings if the
-        apply button was clicked.
+        Handles the button box click event. Updates the knob configuration based on the dialog's settings
+        if the apply button was clicked.
+
+        :param button: The button that was clicked.
         """
         standard_button = self.button_box.standardButton(button)
         if standard_button == QDialogButtonBox.Apply:
             pyfx_log.debug("Knob Config applied")
-            self.knob.set_minimum_value(self.minimum_spinbox.value())
-            self.knob.set_maximum_value(self.maximum_spinbox.value())
-            self.knob.set_default_value(self.default_spinbox.value())
-            self.knob.set_precision(self.precision_spinbox.value())
-            self.knob.set_sensitivity(self.sensitivity_spinbox.value())
-            self.knob.set_mode(self.mode_combobox.currentText())
-            self.knob.set_display_enabled(self.enable_display_checkbox.isChecked())
-
-            if self.knob.maximum_value < self.knob.minimum_value:
-                self.show_invalid_min_max_prompt()
-                return
-
-            if not (self.knob.minimum_value <= self.knob.default_value <= self.knob.maximum_value):
-                self.show_invalid_default_prompt()
-                return
-
-            super().accept()
+            self.apply_changes()
         else:
             pyfx_log.debug("Knob Config aborted")
-            super().reject()
+            self.reject()
+
+    def apply_changes(self):
+        """
+        Applies the changes made in the dialog to the knob.
+        """
+        pyfx_log.debug("Knob Config applied")
+        self.knob.set_minimum_value(self.minimum_spinbox.value())
+        self.knob.set_maximum_value(self.maximum_spinbox.value())
+        self.knob.set_default_value(self.default_spinbox.value())
+        self.knob.set_precision(self.precision_spinbox.value())
+        self.knob.set_sensitivity(self.sensitivity_spinbox.value())
+        self.knob.set_mode(self.mode_combobox.currentText())
+        self.knob.set_display_enabled(self.enable_display_checkbox.isChecked())
+
+        if self.knob.maximum_value < self.knob.minimum_value:
+            self.show_invalid_min_max_prompt()
+            return
+
+        if not (self.knob.minimum_value <= self.knob.default_value <= self.knob.maximum_value):
+            self.show_invalid_default_prompt()
+            return
+
+        super().accept()
 
     def change_mode_settings(self, mode: str):
-        if mode == "logarithmic":
-            self.minimum_spinbox.setSuffix(" dB")
-            self.maximum_spinbox.setSuffix(" dB")
-            self.default_spinbox.setSuffix(" dB")
-            self.precision_spinbox.setSuffix(" dB")
-        else:
-            self.minimum_spinbox.setSuffix("")
-            self.maximum_spinbox.setSuffix("")
-            self.default_spinbox.setSuffix("")
-            self.precision_spinbox.setSuffix("")
+        """
+        Updates the dialog UI based on the selected knob mode.
 
-    def keyPressEvent(self, event):
-        if event.key() in (Qt.Key_Return, Qt.Key_Enter):
-            # Ignore Enter and Return keys
-            event.ignore()
-        else:
-            # Handle other key events normally
-            super().keyPressEvent(event)
+        :param mode: The mode of the knob ('linear' or 'logarithmic').
+        """
+        suffix = " dB" if mode == "logarithmic" else ""
+        self.minimum_spinbox.setSuffix(suffix)
+        self.maximum_spinbox.setSuffix(suffix)
+        self.default_spinbox.setSuffix(suffix)
+        self.precision_spinbox.setSuffix(suffix)
 
     def show_invalid_min_max_prompt(self):
-        pedal_name_missing_prompt = QMessageBox()
-        pedal_name_missing_prompt.setWindowTitle("Invalid Min/Max Values")
-        pedal_name_missing_prompt.setText("The minimum value must be less than the maximum value")
-        pedal_name_missing_prompt.setStandardButtons(QMessageBox.Ok)
-        pedal_name_missing_prompt.exec_()
+        """
+        Shows a prompt indicating that the minimum value must be less than the maximum value.
+        """
+        QMessageBox.warning(self, "Invalid Min/Max Values", "The minimum value must be less than the maximum value")
 
     def show_invalid_default_prompt(self):
-        pedal_name_missing_prompt = QMessageBox()
-        pedal_name_missing_prompt.setWindowTitle("Invalid Default Value")
-        pedal_name_missing_prompt.setText("The default value must be within the minimum and maximum value range")
-        pedal_name_missing_prompt.setStandardButtons(QMessageBox.Ok)
-        pedal_name_missing_prompt.exec_()
+        """
+        Shows a prompt indicating that the default value must be within the min and max value range.
+        """
+        QMessageBox.warning(
+            self, "Invalid Default Value", "The default value must be within the minimum and maximum value range"
+        )
+
+    def keyPressEvent(self, event):  # noqa: N802
+        """
+        Overrides the key press event to ignore Enter and Return keys.
+
+        :param event: The key press event.
+        """
+        if event.key() in (Qt.Key_Return, Qt.Key_Enter):
+            event.ignore()
+        else:
+            super().keyPressEvent(event)
