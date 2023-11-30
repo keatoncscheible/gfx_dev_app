8f7dd6eb9189790581228f6e30162ae765630dc0
keatoncscheible <keatoncscheible@gmail.com>
2023-11-30 03:23:04 -0800

    Doc new_pedal_variant_dialog

--- src/pyfx/widgets/new_pedal_variant_dialog.py
+++ src/pyfx/widgets/new_pedal_variant_dialog.py
@@ -4,13 +4,25 @@ from pyfx.ui.new_pedal_variant_dialog_ui import Ui_NewPedalVariantDialog
 
 
 class NewPedalVariantDialog(QDialog, Ui_NewPedalVariantDialog):
+    """
+    A dialog for creating a new pedal variant.
+    Allows the user to input a name for the new pedal variant.
+    """
+
     def __init__(self):
+        """
+        Initializes the NewPedalVariantDialog.
+        """
         super().__init__()
         self.setupUi(self)
         self.new_pedal_variant = None
 
     def accept(self):
-        new_pedal_variant = self.new_pedal_variant_editbox.text()
+        """
+        Overrides the accept method to include validation of the new pedal variant name.
+        Proceeds with the dialog acceptance only if a valid name is provided.
+        """
+        new_pedal_variant = self.new_pedal_variant_editbox.text().strip()
         if new_pedal_variant:
             self.new_pedal_variant = new_pedal_variant
             super().accept()
@@ -18,8 +30,7 @@ class NewPedalVariantDialog(QDialog, Ui_NewPedalVariantDialog):
             self.show_no_variant_prompt()
 
     def show_no_variant_prompt(self):
-        pedal_name_missing_prompt = QMessageBox()
-        pedal_name_missing_prompt.setWindowTitle("No Variant Provided")
-        pedal_name_missing_prompt.setText("You must set the name of the variant")
-        pedal_name_missing_prompt.setStandardButtons(QMessageBox.Ok)
-        pedal_name_missing_prompt.exec_()
+        """
+        Shows a prompt indicating that a name must be set for the new variant.
+        """
+        QMessageBox.warning(self, "No Variant Provided", "You must set the name of the variant")
